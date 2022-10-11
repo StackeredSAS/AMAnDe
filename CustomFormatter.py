819c2359963db02f547914c691f26284ccb8c1e2
s11ren <115536139+s11ren@users.noreply.github.com>
2022-10-11 22:02:58 +0200

    change logger.py to custoFormatter.py

---
+++ CustomFormatter.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from termcolor import *
+import logging
+import datetime
+
+
+class CustomFormatter(logging.Formatter):
+    def format(self, record):
+        #TODO : updating to python 3.10 before replacing following code with this
+        '''
+        match record.levelno:
+            case logging.INFO:
+                record.msg = colored('[+] %s' % (record.msg),"blue")
+            case logging.WARNING:
+                #record.msg = '[%s] %s' % ("[-]", record.msg)
+                record.msg = colored('[-] %s' % (record.msg),"yellow")
+            case logging.ERROR:
+                record.msg = colored('[!] %s' % (record.msg),"red")
+            case _:
+                print("[!] Logging level not recognized")
+        '''
+
+        #datetime.date.today().strftime("%m/%d %H:%M:%S")
+        if record.levelno == logging.INFO:
+                record.msg = colored('[+] %s' % (record.msg),"blue")
+        elif record.levelno == logging.WARNING:
+                record.msg = colored('[-] %s' % (record.msg), "yellow")
+        elif record.levelno == logging.ERROR:
+                record.msg = colored('[!] %s' % (record.msg), "red")
+        else:
+            print("[!] Logging level not recognized")
+
+        return super(CustomFormatter , self).format(record)
