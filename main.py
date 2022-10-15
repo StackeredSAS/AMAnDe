50245f3e5c32997dcdbf18eb1ea073a2dcaa342b
ENOENT <FlorianPicca@users.noreply.github.com>
2022-10-15 14:22:47 +0200

    fix arguments

--- main.py
+++ main.py
@@ -2,13 +2,14 @@
 import argparse
 from src.parser import Parser
 from src.analyzer import Analyzer
+from src.constants import *
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description='Utility to analyse Android Manifest files.')
     argparser.add_argument('--log-level', '-v', choices=['INFO', 'WARNING', 'ERROR'], help='Sets the log level', default="INFO")
     argparser.add_argument("path", help="The path to the manifest file.")
-    argparser.add_argument("--min-sdk-version", type=int, choices=range(ANDROID_MIN_SDK,ANDROID_MAX_SDK+1), help='Indicate the minimum version supported by your application', metavar='['+str(ANDROID_MIN_SDK)+']', required=True)
-    argparser.add_argument("--max-sdk-version", type=int, choices=range(ANDROID_MIN_SDK,ANDROID_MAX_SDK+1), help='Indicate the maximum version supported by your application', metavar='['+str(ANDROID_MAX_SDK)+']', required=True)
+    argparser.add_argument("--min-sdk-version", type=int, choices=range(ANDROID_MIN_SDK,ANDROID_MAX_SDK+1), help='Indicate the minimum version supported by your application', metavar=f"[{ANDROID_MIN_SDK}, {ANDROID_MAX_SDK}]", required=True)
+    argparser.add_argument("--max-sdk-version", type=int, choices=range(ANDROID_MIN_SDK,ANDROID_MAX_SDK+1), help='Indicate the maximum version supported by your application', metavar=f"[{ANDROID_MIN_SDK}, {ANDROID_MAX_SDK}]", required=True)
     args = argparser.parse_args()
 
     parser = Parser(args.path)
