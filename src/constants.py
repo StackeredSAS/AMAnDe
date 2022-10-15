b7e5a212ec5e9ff54535b7508c14f6e2119d5478
GIT_USERNAME <GIT_EMAIL>
2022-10-15 13:49:27 +0200

    idea for backup and other check which need android SDK version

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,6 @@
+ANDROID_MIN_SDK = 9
+ANDROID_MAX_SDK = 33
+
 dangerous_perms = ['android.permission.ACCEPT_HANDOVER', 
 					'android.permission.ACCESS_BACKGROUND_LOCATION',
 					'android.permission.ACCESS_COARSE_LOCATION',
