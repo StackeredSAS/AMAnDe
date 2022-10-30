1ff0ac465b9af9fb3272bf521e89069f4e379f54
ENOENT <FlorianPicca@users.noreply.github.com>
2022-10-30 21:48:47 +0100

    document config

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 # Config file for external binaries. Useful for Docker integration
 # <name> : <list of arguments to launch the executable> (customizable)
-
+# by default it is assumed the binaries are in your $PATH
 EXTERNAL_BINARIES = {
     "apksigner": ["apksigner"],
 }
