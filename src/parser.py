69ab416f4fd93abbc4dc9d44a64003b682c5873d
ENOENT <FlorianPicca@users.noreply.github.com>
2022-11-01 18:00:28 +0100

    pep8 parser

--- src/parser.py
+++ src/parser.py
@@ -32,6 +32,7 @@ class Parser:
             res = self._getResValue(path, name)
         return res
 
+    # todo : rename and move to utils
     def _getResValue(self, path, name):
         """
         Formats a file name by adding an underline.
@@ -70,7 +71,8 @@ class Parser:
             name = self._getattr(e, "android:name")
             required = str2Bool(self._getattr(e, "android:required"))
             # Default is true for android:required property
-            if required is None: required = True
+            if required is None:
+                required = True
             res.append(UsesLibrary(name, required))
         return res
 
@@ -85,7 +87,8 @@ class Parser:
             name = self._getattr(e, "android:name")
             required = str2Bool(self._getattr(e, "android:required"))
             # Default is true for android:required property
-            if required is None: required = True
+            if required is None:
+                required = True
             res.append(UsesNativeLibrary(name, required))
         return res
 
@@ -100,7 +103,8 @@ class Parser:
             name = self._getattr(e, "android:name")
             required = str2Bool(self._getattr(e, "android:required"))
             # Default is true for android:required property
-            if required is None: required = True
+            if required is None:
+                required = True
             res.append(UsesFeature(name, required))
         return res
 
@@ -154,7 +158,7 @@ class Parser:
 
     def exportedComponents(self, component):
         """
-        Lists all the the exported components of a given type (activity, provider, ...).
+        Lists all the exported components of a given type (activity, provider, ...).
 
         For all components, android:exported is false since Android 4.2. However, 
         if this component specifies an intent filter, android:exported is automatically set to True
@@ -162,16 +166,22 @@ class Parser:
         contains an intent filter (False or True) -> to avoid error. Otherwise, a runtime error will be delivered.
         
         BLOG ARTICLE : https://medium.com/androiddevelopers/lets-be-explicit-about-our-intent-filters-c5dbe2dbdce0
-        An important change is coming to Android 12 that improves both app and platform security. This change affects all apps that target Android 12.
-        Activities, services, and broadcast receivers with declared intent-filters now must explicitly declare whether they should be exported or not.
-        Prior to Android 12, components (activites, services, and broadcast receivers only) with an intent-filter declared were automatically exported
+        An important change is coming to Android 12 that improves both app and platform security. This change affects
+        all apps that target Android 12. Activities, services, and broadcast receivers with declared intent-filters now
+        must explicitly declare whether they should be exported or not. Prior to Android 12, components (activites,
+        services, and broadcast receivers only) with an intent-filter declared were automatically exported
         """
         # check if there is android:exported property set to True (no matter intent filter)
-        exported_component = {self._getattr(e, "android:name") for e in self.root.findall(f'application/{component}[@android:exported="true"]', namespaces=self.namespaces)}
+        exported_component = {self._getattr(e, "android:name") for e in
+                              self.root.findall(f'application/{component}[@android:exported="true"]',
+                                                namespaces=self.namespaces)}
         # check if there is an intent filter in component tag
-        intent_component = {self._getattr(e, "android:name") for e in self.root.findall(f'application/{component}/intent-filter/..', namespaces=self.namespaces)}
+        intent_component = {self._getattr(e, "android:name") for e in
+                            self.root.findall(f'application/{component}/intent-filter/..', namespaces=self.namespaces)}
         # check if there is android:exported property set to False (no matter intent filter)
-        unexported_component = {self._getattr(e, "android:name") for e in self.root.findall(f'application/{component}[@android:exported="false"]', namespaces=self.namespaces)}
+        unexported_component = {self._getattr(e, "android:name") for e in
+                                self.root.findall(f'application/{component}[@android:exported="false"]',
+                                                  namespaces=self.namespaces)}
         # update components (if there is android:exported to False and intent-filter, component is not exported)
         exported_component.update(intent_component - unexported_component)
         return list(exported_component)
@@ -233,25 +243,30 @@ class Parser:
         For ACTIVITY, SERVICE, RECEIVER: android:permission
         For PROVIDER: android:permission, android:grantUriPermissions, android:readPermission, android:writePermission
 
-        android:permission => The name of a permission that clients must have to read or write the content provider's data.
-        This attribute is a convenient way of setting a single permission for both reading and writing. 
-        However, the readPermission, writePermission, and grantUriPermissions (False by default) attributes take precedence 
-        over this one.
+        android:permission :
+        The name of a permission that clients must have to read or write the content provider's data.
+        This attribute is a convenient way of setting a single permission for both reading and writing.
+        However, the readPermission, writePermission, and grantUriPermissions (False by default) attributes
+        take precedence over this one.
         """
 
         # use a namedtuple for more readable access to important attributes
-        ExportedComponents = namedtuple("ExportedComponents", "componentName componentType permission readPermission writePermission grantUriPermissions")
+        ExportedComponents = namedtuple("ExportedComponents",
+                                        "componentName componentType permission readPermission "
+                                        "writePermission grantUriPermissions")
         res = []
         for name in self.exportedComponents(componentType):
-            component = self.root.find(f'application/{componentType}[@android:name="{name}"]', namespaces=self.namespaces)
+            component = self.root.find(f'application/{componentType}[@android:name="{name}"]',
+                                       namespaces=self.namespaces)
             permission = self._getattr(component, "android:permission")
             readPermission, writePermission, grantUriPermissions = None, None, None
-            if (componentType == "provider"):
+            if componentType == "provider":
                 # only providers have those attributes
                 readPermission = self._getattr(component, "android:readPermission")
                 writePermission = self._getattr(component, "android:writePermission")
                 grantUriPermissions = str2Bool(self._getattr(component, "android:grantUriPermissions"))
-            res.append(ExportedComponents(name, componentType, permission, readPermission, writePermission, grantUriPermissions))
+            res.append(ExportedComponents(name, componentType, permission, readPermission, writePermission,
+                                          grantUriPermissions))
         return res
 
     def getUnexportedProviders(self):
@@ -262,16 +277,21 @@ class Parser:
         https://blog.oversecured.com/Android-Access-to-app-protected-components/
         https://snyk.io/blog/exploring-android-intent-based-security-vulnerabilities-google-play/
         """
-        return {self._getattr(e, "android:name") for e in self.root.findall(f'application/provider[@android:grantUriPermissions="true"][@android:exported="false"]', namespaces=self.namespaces)}
+        return {self._getattr(e, "android:name") for e in self.root.findall(
+            f'application/provider[@android:grantUriPermissions="true"][@android:exported="false"]',
+            namespaces=self.namespaces)}
 
     def getIntentFilterExportedComponents(self):
         """
         Returns a tuple (componentName, componentType) for each exported component having
         one or more intent_filter(s) (android:exported is true or none)
         """
-        all_intent = {(self._getattr(e, "android:name"), e.tag) for e in self.root.findall(f'application/*/intent-filter/..')}
-        not_exported = {(self._getattr(e, "android:name"), e.tag) for e in self.root.findall(f'application/*[@android:exported="false"]/intent-filter/..', namespaces=self.namespaces)}
-        return all_intent-not_exported
+        all_intent = {(self._getattr(e, "android:name"), e.tag) for e in
+                      self.root.findall(f'application/*/intent-filter/..')}
+        not_exported = {(self._getattr(e, "android:name"), e.tag) for e in
+                        self.root.findall(f'application/*[@android:exported="false"]/intent-filter/..',
+                                          namespaces=self.namespaces)}
+        return all_intent - not_exported
 
     def getIntentFilters(self, compname):
         """
@@ -279,7 +299,8 @@ class Parser:
         from an Element with given name.
         """
         # get intent-filter element from a Element with given name
-        intents = self.root.findall(f"application/*[@android:name=\"{compname}\"]/intent-filter", namespaces=self.namespaces)
+        intents = self.root.findall(f"application/*[@android:name=\"{compname}\"]/intent-filter",
+                                    namespaces=self.namespaces)
         res = []
         # each intent on a separated line
         for e in intents:
@@ -359,10 +380,13 @@ class Parser:
         deepLinks = []
         for compname, tag in exported_components:
             # deep links must have ACTION_VIEW
-            intents = self.root.findall(f'application/*[@android:name=\"{compname}\"]/intent-filter/action[@android:name="android.intent.action.VIEW"]/..', namespaces=self.namespaces)
+            intents = self.root.findall(f'application/*[@android:name=\"{compname}\"]/intent-filter/'
+                                        f'action[@android:name="android.intent.action.VIEW"]/..',
+                                        namespaces=self.namespaces)
             for i in intents:
                 # deep links must have category BROWSABLE
-                if i.find('category[@android:name="android.intent.category.BROWSABLE"]', namespaces=self.namespaces) is not None:
+                if i.find('category[@android:name="android.intent.category.BROWSABLE"]',
+                          namespaces=self.namespaces) is not None:
                     mimeType = {self._getattr(e, "android:mimeType") for e in i.findall("data")}
                     uris = self._getIntentFiltersUrisInfo(i, len(mimeType) > 1)
                     # add additional info to check if a deeplink is actually an app link
